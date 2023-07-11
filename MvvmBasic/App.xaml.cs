bf96ce03c4fe0f50dd2970f9c4148592de0f9f93
Inoue Shinichi <inoue.shinichi.1800@gmail.com>
2023-07-11 11:28:34 +0900

    [Add] MVVM

---
+++ MvvmBasic/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MvvmBasic
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MvvmBasic/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿namespace MvvmBasic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+public partial class MainPage : ContentPage
+{
+     public MainPage()
+     {
+        InitializeComponent();
+        this.Loaded += (_, _) =>
+        {
+            this.BindingContext = _vm;
+        };
+     }
+
+     MyViewModel _vm = new MyViewModel();
+}
+
+// ViewModel
+public class MyViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    private string _hello = "Hello, world!";
+    public string Hello
+    {
+        get => _hello;
+        set
+        {
+            if (_hello != value)
+            {
+                _hello = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public void OnPropertyChanged(
+        [CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(
+                this, new PropertyChangedEventArgs(name));
+
+    public ICommand OnHelloClicked { get; private set; }
+    public MyViewModel()
+    {
+        OnHelloClicked = new Command(() =>
+        {
+            this.Hello = "ようこそ .NET MAUIの世界へ";
+        });
+    }
+
+}
